cb5edb5109a6d673aa99dee89c393db94874188c
numarako <s12257010@gmail.com>
2021-09-22 08:28:04 +0900

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,15 +19,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "valid signup information" do
-    get signup_url
-    assert_difference "User.count", 1 do
-      post users_path, params: { user: { name: "nao",
-                                 email: "user@gmail.com",
-                                 password: "foobar",
-                                 password_confirmation: "foobar" } }
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
