dc5b0a5f2bfc83f80ac14997fca61251eb012945
numarako <s12257010@gmail.com>
2021-09-21 12:29:37 +0900

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: { user: { name: "",
+                                 email: "user@invalid",
+                                 password: "foo",
+                                 password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
+    assert_select 'div.alert'
+  end
+
+  test "valid signup information" do
+    get signup_url
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { name: "nao",
+                                 email: "user@gmail.com",
+                                 password: "foobar",
+                                 password_confirmation: "foobar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
