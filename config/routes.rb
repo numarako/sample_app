8c26761e5ad05179b1002672496a12c9a9f639a4
numarako <s12257010@gmail.com>
2021-10-04 19:36:12 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     member do
       get :following, :followers
     end
-  end
+  end 
   resources :relationships,       only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root 'application#hello'
